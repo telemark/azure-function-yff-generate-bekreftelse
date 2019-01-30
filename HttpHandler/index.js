6f1cce473ec9b78cb5612bcb253d9310e9de3608
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-01-30 14:25:43 +0100

    use ContentData

--- HttpHandler/index.js
+++ HttpHandler/index.js
@@ -1,9 +1,13 @@
 const prepareDocument = require('../lib/prepare-document')
 const generateDocumentData = require('../lib/generate-document-data')
 
+function base64ToObj (base64) {
+  return JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'))
+}
+
 module.exports = async function (context, request) {
   if (request.body) {
-    let body = request.body
+    let body = base64ToObj(request.body.ContentData)
     const data = await prepareDocument(body)
     const document = await generateDocumentData(context, data)
     body.document = document
